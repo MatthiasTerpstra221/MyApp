6e837ef1dae1f00c48077e24f820afb6517ae975
MatthiasTerpstra221 <matthias.terpstra@leapforce.nl>
2025-04-14 18:54:29 +0200

    Update Header.jsx

--- src/components/Calculator/Header.jsx
+++ src/components/Calculator/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = () => {
+export const Header = () => {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
@@ -43,7 +43,3 @@ const Header = () => {
     </div>
   );
 };
-
-// Add both default and named exports
-export { Header };
-export default Header;
